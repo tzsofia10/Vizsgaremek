77cc0ec3c4b530c0bb77493012ccb0d71769a534
TothZsofia <tzsofia07@gmail.com>
2025-01-28 10:06:39 +0100

    jo

--- main/nav.php
+++ main/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="#">Főoldal</a>
-    <a href="#napi-bejegyzesek">Napi bejegyzések</a>
+    <a href="../index.php">Napi bejegyzések</a>
     <a href="#statisztikak">Statisztikák</a>
     <a href="#kapcsolat">Kapcsolat</a>
     <a href="../admin/index.php">Bejelentkezés</a>
