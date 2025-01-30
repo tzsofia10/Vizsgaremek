92fad188430151883c8d2a3c126a0cda16e9d8e1
Zsófia Szofia <tzsofia07@gmail.com>
2025-01-30 09:25:02 +0100

    ÁÁÁÁÁÁÁÁÁÁ BUTTON

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,31 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    
+
+    // Az összes kép elrejtése
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
 
+    // Index növelése, ha elérte a maximumot, újraindul
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+
+    // Aktuális kép megjelenítése
+    slides[slideIndex - 1].style.display = "block";  
+
+    // Pontok aktív állapotának frissítése
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+    dots[slideIndex - 1].className += " active";
 
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    // Automatikus lapozás 3 másodpercenként
+    setTimeout(showSlides, 3000);
 }
+
+// Indítsa el a diavetítést az oldal betöltésekor
+document.addEventListener("DOMContentLoaded", showSlides);
--- main/nav.php
+++ main/nav.php
@@ -2,8 +2,20 @@
     <div class="logo-container">
         <img src="../cowPicture/logo.png" alt="Logo">
     </div>
-    <a href="../main/index.php">Főoldal</a>
-    <a href="../index.php">Napi bejegyzések</a>
-    <a href="#statisztikak">Statisztikák</a>
-    <a href="../admin/index.php">Bejelentkezés</a>
+    <div class="nav-links">
+        <a href="../main/index.php">Főoldal</a>
+        <a href="../index.php">Napi bejegyzések</a>
+        <a href="#statisztikak">Statisztikák</a>
+    </div>
+    <div class="button-container">
+        <button>
+            <a href="../admin/index.php">
+                
+                Sign up
+            </a>
+            <div class="arrow-wrapper">
+                <div class="arrow"></div>
+            </div>
+        </button>
+    </div>
 </nav>
