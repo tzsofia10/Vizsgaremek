3cc3ee40b2debef3ac9bbaff719da5947f424382
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2025-01-30 09:25:42 +0100

    fixing

--- admin/farm_states.php
+++ admin/farm_states.php
@@ -87,8 +87,12 @@ $result = $dbconn->query($sql);
                         <td>" . htmlspecialchars($row['birth_date']) . "</td>
                         <td>
                             <button class='deleteButton'>
-                                <a href='delete_cow.php?id=" . urlencode($row['cow_id']) . "'>Törlés</a>
+                                <a href='delete_cow.php?id=" . urlencode($row['cow_id']) . "'>
+                                    <svg viewBox='0 0 448 512' class='svgIcon'><path d='M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z'></path></svg>
+                                </a>
                             </button>
+                    
+                    
                             <button class='editButton'>
                                 <a href='edit_cow.php?id=" . urlencode($row['cow_id']) . "'>Módosítás</a>
                             </button>
