49d4c1b0bab640e6c67b67bbdebdfbbb78a69fa6
zsiros kata <zsiros.kata02>
2025-02-13 13:42:37 +0100

    szar a nav AGAIN

--- main/nav.php
+++ main/nav.php
@@ -3,17 +3,19 @@
         <img src="../cowPicture/logo.png" alt="Logo">
     </div>
     <div class="nav-links">
-        <a href="../main/index.php">Főoldal</a>
-        <a href="../index.php">Napi bejegyzések</a>
-        <a href="#statisztikak">Statisztikák</a>
-    </div>
-    <div class="button-container">
-        <button>
-            <a href="../admin/index.php">Sign up</a>
-            <div class="arrow-wrapper">
-                <div class="arrow"></div>
-            </div>
-        </button>
+        <div class="links">
+            <a href="../main/index.php">Főoldal</a>
+            <a href="../index.php">Napi bejegyzések</a>
+            <a href="#statisztikak">Statisztikák</a>
+        </div>
+        <div class="button-container">
+            <button>
+                <a href="../admin/index.php">Bejelentkezés</a>
+                <div class="arrow-wrapper">
+                    <div class="arrow"></div>
+                </div>
+            </button>
+        </div>
     </div>
     <div class="hamburger" onclick="toggleMenu()">
         <div></div>
