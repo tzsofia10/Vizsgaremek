5c6c2f8c923aaf2ae593a8103a06838205ecd38b
TothZsofia <tzsofia07@gmail.com>
2025-03-24 11:03:28 +0100

    qwe

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ include 'main/head.php';
     <!-- Constant buttons below the images -->
     <div class="position-absolute bottom-0 start-50 translate-middle-x mb-3">
         <button class="btn btn-primary me-2" id="vacinationButton"><a href="vacination.php"> Oltások</a> </button>
-        <button id="playSound" class="btn btn-secondary">Múúúú</button>
+        <button id="playSound" class="btn btn-secondary moo">Múúúú</button>
         <audio id="audio" src="./hang mu/cowmu.mp3"></audio> 
         
 
