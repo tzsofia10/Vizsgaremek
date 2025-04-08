bc31119fde04ce23274fdfa160c5e59f02c76892
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2025-04-08 09:59:55 +0200

    farm

--- admin/farm_states.php
+++ admin/farm_states.php
@@ -115,48 +115,53 @@ $result = $dbconn->query($sql);
                             <th>Nem</th>
                             <th>Anya fülszám</th>
                             <th>Szín</th>
-                            <th>Születési dátum</th>
+                            <th>" . ($filter === 'dead' ? 'Elhullás dátuma' : 'Születési dátum') . "</th>
+                            " . ($filter === 'all' ? "<th>Státusz</th>" : "") . "
                             <th colspan='2'>Műveletek</th>
                         </tr>
                     </thead>
                     <tbody>";
+        
     
-            while ($row = $result->fetch_assoc()) {
-                $color = $row['color'] ?? 'N/A';
-                if ($row['gender'] == 1) {
-                    $gender = "Hím";
-                } else {
-                    $gender = "Nőstény";
-                }
-
-                $picture = !empty($row['picture']) ? htmlspecialchars($row['picture']) : '../cowPicture/nopicture.jpg';
-    
-                echo "<tr>
-                        <td>" . htmlspecialchars($row['cow_id']) . "</td>
-                        <td><img src='" . $picture . "' alt='Tehén Kép'></td>
-                        <td>" . htmlspecialchars($row['ear_tag']) . "</td>
-                        <td>" . htmlspecialchars($gender) . "</td>
-                        <td>" . htmlspecialchars($row['mother_ear_tag']) . "</td>
-                        <td class='color'>" . htmlspecialchars($color) . "</td>
-                        <td>" . htmlspecialchars($row['birthdate']) . "</td>
-                        <td class='btn-update borderRight'>
-                            <a href='edit_cow.php?id=" . urlencode($row['cow_id']) . "'>
-                                <button class='edit-button'>
-                                    <svg class='edit-svgIcon' viewBox='0 0 512 512'>
-                                        <path d='M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z'></path>
+                    while ($row = $result->fetch_assoc()) {
+                        $color = $row['color'] ?? 'N/A';
+                        $gender = $row['gender'] == 1 ? "Hím" : "Nőstény";
+                        $picture = !empty($row['picture']) ? htmlspecialchars($row['picture']) : '../cowPicture/nopicture.jpg';
+                        $status = $row['death_date'] ? 'Elhullott' : 'Életben van';
+                        $status_class = $row['death_date'] ? 'dead' : 'alive';
+                    
+                        echo "<tr>
+                            <td>" . htmlspecialchars($row['cow_id']) . "</td>
+                            <td><img src='" . $picture . "' alt='Tehén Kép'></td>
+                            <td>" . htmlspecialchars($row['ear_tag']) . "</td>
+                            <td>" . htmlspecialchars($gender) . "</td>
+                            <td>" . htmlspecialchars($row['mother_ear_tag']) . "</td>
+                            <td class='color'>" . htmlspecialchars($color) . "</td>
+                            <td>" . htmlspecialchars($row['birthdate']) . "</td>";
+                    
+                        if ($filter === 'all') {
+                            echo "<td class='" . $status_class . "'>" . $status . "</td>";
+                        }
+                    
+                        echo "<td class='btn-update borderRight'>
+                                <a href='edit_cow.php?id=" . urlencode($row['cow_id']) . "'>
+                                    <button class='edit-button'>
+                                            <svg class='edit-svgIcon' viewBox='0 0 512 512'>
+                                                <path d='M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z'></path>
+                                            </svg>
+                                    </button>
+                                </a>
+                            </td>
+                            <td class='btn-update borderRight'>
+                                <button class='deleteButton' data-delete-url='delete_cow.php?id=" . urlencode($row['cow_id']) . "'>
+                                    <svg viewBox='0 0 448 512' class='svgIcon'>
+                                        <path d='M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z'></path>
                                     </svg>
                                 </button>
-                            </a>
-                        </td>
-                        <td class='btn-update borderRight'>
-                            <button class='deleteButton' data-delete-url='delete_cow.php?id=" . urlencode($row['cow_id']) . "'>
-                                <svg viewBox='0 0 448 512' class='svgIcon'>
-                                    <path d='M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z'></path>
-                                </svg>
-                            </button>
-                        </td>
-                    </tr>";
-            }
+                            </td>
+                        </tr>";
+                    }
+                    
     
             echo "</tbody></table>";
 
